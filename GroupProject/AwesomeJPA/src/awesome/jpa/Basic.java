bcecf546b1362d7f352aa0e7dcfdaddf885e71a6
James Hurford <terrasea@gmail.com>
2011-05-17 22:59:41 +1200

    Made Basic usable by at run time

--- GroupProject/AwesomeJPA/src/awesome/jpa/Basic.java
+++ GroupProject/AwesomeJPA/src/awesome/jpa/Basic.java
@@ -1,5 +1,12 @@
 package awesome.jpa;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Basic {
 	FetchType fetch() default FetchType.LAZY;
 	boolean optional() default true;

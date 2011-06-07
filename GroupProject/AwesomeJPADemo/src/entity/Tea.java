2a407759503c9a7f7106d9199e1c5af820c729ae
James Hurford <terrasea@gmail.com>
2011-06-07 22:56:32 +1200

    A demo project for our AwesomeJPA library

---
+++ GroupProject/AwesomeJPADemo/src/entity/Tea.java
@@ -0,0 +1,41 @@
+package entity;
+
+import awesome.persistence.annotations.Basic;
+
+public class Tea {
+	@Basic
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Basic
+	int strength;
+	
+	public int getStrength() {
+		return strength;
+	}
+	
+	
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	
+	
+	@Basic
+	boolean milk;
+	
+	public boolean getMilk() {
+		return milk;
+	}
+	
+	
+	public void setMilk(boolean milk) {
+		this.milk = milk;
+	}
+}
---
+++ GroupProject/AwesomeJPADemo/src/main/Main.java
@@ -0,0 +1,111 @@
+package main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import entity.Coffee;
+import entity.Tea;
+
+import awesome.persistence.agent.AgentException;
+import awesome.persistence.agent.LazyInitAgent;
+import awesome.persistence.agent.Transformer;
+import awesome.persistence.manager.AQLException;
+import awesome.persistence.manager.EntityException;
+import awesome.persistence.manager.Manager;
+import awesome.persistence.manager.NotAEntity;
+import awesome.persistence.manager.PropertiesException;
+import awesome.persistence.test.Primatives;
+
+public class Main {
+
+	private static String propertiesPath = "lib/awesome.properties";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LazyInitAgent agent = new LazyInitAgent();
+		agent.addEntity("Coffee");
+		agent.addEntity("Tea");
+		Transformer.addTransformer(agent);
+		try {
+			Transformer.startAgent();
+		} catch (AgentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Manager.setProperties(propertiesPath);
+			Coffee coffee = new Coffee();
+			coffee.setName("Long black");
+			coffee.setStrength(5);
+			coffee.setMilk(false);
+			
+			Tea tea = new Tea();
+			tea.setName("Earl gray");
+			tea.setStrength(4);
+			tea.setMilk(true);
+			
+			
+			Manager.persist(coffee);
+			Manager.persist(tea);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PropertiesException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotAEntity e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		List<Object> results = null;
+		try {
+			results = Manager.queryDB("FETCH " + Coffee.class.getName());
+			for(int index = 0; index < results.size(); index++){
+				Primatives res = (Primatives) results.get(index);
+				System.out.println(res.getPString());
+			}
+		} catch (AQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+				
+		try {
+			results = Manager.queryDB("FETCH " + Tea.class.getName());
+			for(int index = 0; index < results.size(); index++){
+				Primatives res = (Primatives) results.get(index);
+				System.out.println(res.getPString());
+			}
+		} catch (AQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}

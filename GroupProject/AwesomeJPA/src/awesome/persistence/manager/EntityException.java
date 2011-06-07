52da99eb1cff033b4afeecf3b67e1b3dead3d1ac
James Hurford <terrasea@gmail.com>
2011-06-07 15:52:39 +1200

    Added EntityException

---
+++ GroupProject/AwesomeJPA/src/awesome/persistence/manager/EntityException.java
@@ -0,0 +1,14 @@
+package awesome.persistence.manager;
+
+public class EntityException extends Exception {
+
+	public EntityException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
